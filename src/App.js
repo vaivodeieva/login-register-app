ef5cf13187870aba9cffee17efeeaf62134759b4
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-07-10 18:50:39 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import "./App.css";
+import { useSpring, animated } from "react-spring";
+
+function App() {
+  const [registrationFormStatus, setRegistartionFormStatus] = useState(false);
+  const loginProps = useSpring({
+    left: registrationFormStatus ? -500 : 0, // Login form sliding positions
+  });
+  const registerProps = useSpring({
+    left: registrationFormStatus ? 0 : 500, // Register form sliding positions 
+  });
+
+  const loginBtnProps = useSpring({
+    borderBottom: registrationFormStatus
+      ? "solid 0px transparent"
+      : "solid 2px #1059FF",  //Animate bottom border of login button
+  });
+  const registerBtnProps = useSpring({
+    borderBottom: registrationFormStatus
+      ? "solid 2px #1059FF"
+      : "solid 0px transparent", //Animate bottom border of register button
+  });
+
+  function registerClicked() {
+    setRegistartionFormStatus(true);
+  }
+  function loginClicked() {
+    setRegistartionFormStatus(false);
+  }
+
+  return (
+    <div className="login-register-wrapper">
+      <div className="nav-buttons">
+        <animated.button
+          onClick={loginClicked}
+          id="loginBtn"
+          style={loginBtnProps}
+        >
+          Login
+        </animated.button>
+        <animated.button
+          onClick={registerClicked}
+          id="registerBtn"
+          style={registerBtnProps}
+        >
+          Register
+        </animated.button>
+      </div>
+      <div className="form-group">
+        <animated.form action="" id="loginform" style={loginProps}>
+          <LoginForm />
+        </animated.form>
+        <animated.form action="" id="registerform" style={registerProps}>
+          <RegisterForm />
+        </animated.form>
+      </div>
+      <animated.div className="forgot-panel" style={loginProps}>
+        <a herf="#">Forgot your password?</a>
+      </animated.div>
+    </div>
+  );
+}
+
+function LoginForm() {
+  return (
+    <React.Fragment>
+      <label for="username">USERNAME</label>
+      <input type="text" id="username" required />
+      <label for="password">PASSWORD</label>
+      <input type="text" id="password" required />
+      <input type="submit" value="submit" className="submit" />
+    </React.Fragment>
+  );
+}
+
+function RegisterForm() {
+  return (
+    <React.Fragment>
+      <label for="username">username</label>
+      <input type="text" id="username" required />
+
+      <label for="password">password</label>
+      <input type="text" id="password" required />
+
+      <label for="confirmpassword">confirm password</label>
+      <input type="text" id="confirmpassword" required/>
+
+      <label for="firstname">first name</label>
+      <input type="text" id="firstname" required />
+
+      <label for="lastname">last name</label>
+      <input type="text" id="lastname" />
+
+      <select className="form-select" aria-label="Default select example" name="country"
+        id="input-country-field">
+        <option selected disabled required >Country*</option>
+        <option value="1">Latvia</option>
+        <option value="2">Belgium</option>
+        <option value="3">France</option>
+      </select>
+
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="1" name="agree"
+          id="checkbox-terms-conditions"/>
+          <label className="form-check-label" for="checkbox-terms-conditions">
+            Agree with terms and conditions
+          </label>
+                                    </div>
+
+
+        {/* <label for="email">email</label>
+      <input type="text" id="email" /> */}
+
+
+        <input type="submit" value="submit" class="submit" />
+    </React.Fragment>
+      );
+}
+
+      export default App;
